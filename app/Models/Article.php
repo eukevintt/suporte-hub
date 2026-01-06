a53c6db3ead79107b9adc89ced5f6a626fce2106
Kevin Araujo <kevinaraujoo677@gmail.com>
2026-01-06 20:19:14 -0300

    feat: add article model and relationships

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'status',
+        'category_id',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
