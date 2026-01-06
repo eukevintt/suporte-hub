2ec755638f67cd51ffce45a9cc8de77c3343b358
Kevin Araujo <kevinaraujoo677@gmail.com>
2026-01-06 13:47:16 -0300

    feat: share generated password flash via inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,9 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'flash' => [
+                'generated_password' => fn () => $request->session()->get('generated_password'),
+            ],
         ];
     }
 }
